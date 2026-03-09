67a032e3341f11dbf1680b13a726739a0f9d177e
deporccs <119077114+deporccs@users.noreply.github.com>
2026-03-09 21:54:53 +0000

    Fix total de rutas relativas

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Ajustado al nombre real de tu repositorio para evitar el error 404
-  base: '/FIXTURE/', 
+  base: '/gestor_torneo/',
 })
